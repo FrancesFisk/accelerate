5d044fab0649095ca9d40900faf730bcbc769136
Frances Fisk <frances.id@gmail.com>
2017-06-21 11:41:40 -0700

    responsive styles

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -18,7 +18,7 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='homepage-hero'>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
+				<a class="button" href="<?php echo home_url(); ?>/case-studies">View Our Work</a>
 			</div>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .container -->
@@ -45,13 +45,14 @@ get_header(); ?>
 	</div>
 </section>
 
+<div class="homepage-posts">
 <section class="recent-posts">
 	<div class="site-content">
 		<div class="blog-post">
 			<h4>From the blog</h4>
 			<?php query_posts('posts_per_page=1'); ?>
 				<?php while (have_posts()) : the_post(); ?>
-					<h2><?php the_excerpt(); ?></h2>
+					<h2><?php the_title(); ?></h2>
 					<?php the_excerpt(); ?>
 						<a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
 				<?php endwhile; ?>
@@ -59,5 +60,20 @@ get_header(); ?>
 		</div>
 	</div>
 </section>
+
+<section class="front-sidebar">
+	<div class="site-content">
+		<div class="twitter-widget">
+			<h4>Recent Tweet</h4>
+			<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+				<div id="secondary" class="widget-area" role="complementary">
+					<?php dynamic_sidebar( 'sidebar-2' ); ?>
+				</div>
+				<a href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Flocalhost%3A8888%2Faccelerate%2F%3Fpreview_id%3D5%26preview_nonce%3D4f03b67e78%26preview%3Dtrue&ref_src=twsrc%5Etfw&region=follow_link&screen_name=francesid&tw_p=followbutton" target="_blank" id="follow-twitter">Follow Us <span>&rsaquo;</span></a>
+			<?php endif; ?>
+		</div>
+	</div>
+</section>
+</div>
 
 <?php get_footer(); ?>
--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -25,6 +25,8 @@
  * @since Accelerate Marketing 1.0
  */
 
+ /* CUSTOME POST TYPES */
+
  function create_custom_post_types() {
     register_post_type( 'case_studies',
         array(
@@ -50,3 +52,20 @@
     );
 }
 add_action( 'init', 'create_custom_post_types' );
+
+/* WIDGET */
+
+function accelerate_theme_child_widget_init() {
+
+	register_sidebar( array(
+	    'name' =>__( 'Homepage sidebar', 'accelerate-theme-child'),
+	    'id' => 'sidebar-2',
+	    'description' => __( 'Appears on the static front page template', 'accelerate-theme-child' ),
+	    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	    'after_widget' => '</aside>',
+	    'before_title' => '<h3 class="widget-title">',
+	    'after_title' => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
